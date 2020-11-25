ac9e3ad8f26c3a6148219b1d057872798709cd3e
Charles Haller <c.haller@ec2e.com>
2020-11-25 13:49:01 +0100

    Vue SPA with Laravel

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+import Vue from 'vue'
+
+import ExampleComponent from './components/ExampleComponent.vue';
+Vue.component('example-component', ExampleComponent);
+
+const app = new Vue({
+    el: '#app'
+});
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="row ml-4 mr-4">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    Graphique 1 (BarChart)
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Example</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>=
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    Graphique 2 (PieChart)
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Example</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        components: {},
+        data() {
+            return {
+
+            }
+        },
+        mounted() {
+            this.fillData()
+        },
+        methods: {
+            fillData() {
+                // Appeler un route pour récupérer les données
+            },
+        }
+    }
+</script>
+
+<style>
+</style>
