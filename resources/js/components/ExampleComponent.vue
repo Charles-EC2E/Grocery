1d86a8af7bc5aa1dd994959a08ad200bb85dd7a0
LeoBruant <bruantleo@gmail.com>
2020-12-08 20:43:06 +0100

    ajout du composant table

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -6,15 +6,19 @@
         <div class="col-md-6">
             <pie-chart-component :title="'PieChart'"></pie-chart-component>
         </div>
+        <div class="col-md-6">
+            <table-component></table-component>
+        </div>
     </div>
 </template>
 
 <script>
     import BarChartComponent from './BarChartComponent.vue'
     import PieChartComponent from './PieChartComponent.vue'
+    import TableComponent from './TableComponent.vue'
 
     export default {
-        components: {BarChartComponent, PieChartComponent},
+        components: {BarChartComponent, PieChartComponent, TableComponent},
         data() {
             return {
             }
